58f36011b8213f3e0f21a8647ea0d3562753c30f
b18jenli <49142647+b18jenli@users.noreply.github.com>
2021-04-12 14:51:58 +0200

    Some fixes (resp)

--- fixed.js
+++ fixed.js
@@ -107,7 +107,10 @@ function showpage(pageid){
         pages[i].style.display="none";
     }
     document.getElementById(pageid).style.display="block";
+    menuFunction()
     }
+
+
 
 function myDropdown(){
     document.getElementById("myDropdown").classList.toggle("show");
@@ -129,29 +132,6 @@ window.onclick = function(e){
     }
 }
         
-var slideIndex = -1;
-showSlides()
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length){
-        slideIndex = 1
-    }
-    for (i = 0; i < dots.length; i++){
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 3000); 
-}
-
-
   function menuFunction() {
                 var x = document.getElementById("menu");
                 if (x.className === "navbar") {
